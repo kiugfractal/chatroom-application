e42d144721bfd15aed8baef45c5f0ed2f233f496
Phuc Doan <kiugfractal161@gmail.com>
2012-12-20 23:30:23 +0200

    add keyboard enter function to the application

--- testing/public/myScript.js
+++ testing/public/myScript.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	var socket = io.connect('http://localhost:3000');
 
@@ -21,6 +20,16 @@ $(document).ready(function(){
 	}
 
 
+	//fetch the message to server and append message to view 
+	function fetchMessage(){
+		//send the message to server 
+		socket.send(getMessage());
+		//append the message to the user itself 
+		$('#message').append('<li class="yourself">'+getMessage()+'</li>');
+		$('#messageText').val('');
+		$('#message').listview('refresh');
+	}
+
 	//listen when server broadcast
 	socket.on('message',function(message){
 		$('#message').append('<li>'+message+'</li>');
@@ -30,11 +39,23 @@ $(document).ready(function(){
 
 	//send message to server 
 	$('#sendButton').on('click',function(){
-		//send the message to server 
-		socket.send(getMessage());
-		//append the message to the user itself 
-		$('#message').append('<li class="yourself">'+getMessage()+'</li>');
-		$('#messageText').val('');
-		$('#message').listview('refresh');
+		fetchMessage();
 	});
+
+	//for keyboard 
+	//function: press enter for clicking button, input: id of input and button 
+	function enterForm(idInput,idButton){
+		$(idInput).on('keypress',function(e){
+			if (e.keycode == 13){
+				$(idButton).click();
+			}
+		});
+	}
+
+	//submit page
+	enterForm('#usernameText','#submitButton');
+	
+	//chat page when user enter message 
+	enterForm('#messageText','#sendButton');
+
 });
